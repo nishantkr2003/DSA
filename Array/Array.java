8c21245af0785aaf85e61deee9028280cad224d7
Nishant kumar <nishantkr2003nna@gmail.com>
2025-03-04 08:24:05 +0530

    Array

---
+++ Array/Array.java
@@ -0,0 +1,15 @@
+public class Array{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+
+        // Array Declaration
+
+        //datatype[] var_name = new datatype[size];
+
+        int[]  rnos1 = new int[5];
+        String[] arr = new  String[5];
+        System.out.println(arr[0]);
+
+        int[] rnos2 = {23,24,25,26,27}; 
+    }
+}
