1e02d35b8fa6f398aae3bca6af219a71cfd78acc
Nishant kumar <nishantkr2003nna@gmail.com>
2025-03-25 11:36:44 +0530

    Binary Search

---
+++ Searching/Binary Search/BinarySearch.java
@@ -0,0 +1,36 @@
+public class BinarySearch{
+    public static void main(String[] args) {
+        int[] arr = {-8,25,-6,-45,-254,25,51 ,55,585};
+        int target = 25;
+        int result = binarySearch(arr, target);
+        System.err.println(result);
+
+
+
+        // return index
+        // return -1 if not in arr
+
+    
+
+    }
+
+     static int binarySearch(int[] arr, int x) {
+            int start=0;
+            int end =arr.length-1;
+
+            while(start<=end){
+                int mid = (start+end)/2;
+
+                if(x< arr[mid]){
+                    end = mid-1;
+                }
+                else if(x>arr[mid]){
+                    start=mid+1;
+                }
+                else{
+                    return mid;
+                }
+            }
+            return -1;
+        }
+}
