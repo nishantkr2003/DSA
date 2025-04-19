a67b142b76a4f8d6ac740c1d306a583a744f441b
Nishant kumar <nishantkr2003nna@gmail.com>
2025-04-19 22:32:28 +0530

    Create 136.SingleNumber.java

---
+++ LeetCode/136.SingleNumber.java
@@ -0,0 +1,14 @@
+
+//https://leetcode.com/problems/single-number/
+
+
+class Solution {
+
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for(int i=0;i<nums.length;i++){
+            result = result^nums[i];
+        }
+        return result;
+    }
+}
