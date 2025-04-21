8fa3bb48dc2f234823bd336a9a2c4fd2d9668d5f
Nishant kumar <nishantkr2003nna@gmail.com>
2025-04-21 16:19:43 +0530

    String

---
+++ String/Compare.java
@@ -0,0 +1,19 @@
+
+
+public class Compare {
+
+    public static void main(String[] args) {
+        String a = "nishant";
+        String b = "nishant";
+
+        System.out.println(a == b); // true, because both refer to the same object in the string pool
+
+        String name1 = new String("nishant");
+        String name2 = new String("nishant");
+        System.out.println(name1 == name2); // false, because they are different objects in the heap
+
+
+        System.out.println(name1.equals(name2)); // true, because they have the same content
+        System.out.println(name1.equals(a)); // true, because they have the same content
+    }
+}
---
+++ String/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String a = "nishant";
+        System.out.println(a);
+        a = "kumar";
+        System.out.println(a);
+    }
+}
