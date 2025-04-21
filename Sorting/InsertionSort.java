4986cd4aef51a18f9c4861c85176ed58fc1f4f0c
Nishant kumar <nishantkr2003nna@gmail.com>
2025-04-21 07:50:05 +0530

    Create InsertionSort.java

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,41 @@
+package Sorting;
+import java.util.*;
+
+public class InsertionSort {
+
+    public static void main(String[] args) {
+        int arr[] = {5,4,3,2,1,0,85,-12};
+        insertion(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void insertion(int arr[]){
+        for(int i=1;i<arr.length;i++){
+            int temp = arr[i];
+            int j = i-1;
+            while(j>=0 && arr[j]>temp){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = temp;
+        }
+
+    }
+
+    public static void insertionSort(int[] arr) {
+        for(int i=1;i<arr.length;i++){
+            int curr = arr[i];
+            int prev = i-1;
+
+            //finding out the correct pos to insert
+            while(prev>=0 && arr[prev]>curr){
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+            //inserting the element at the correct position
+
+            arr[prev+1] = arr[curr];
+        }
+    }
+    
+}
