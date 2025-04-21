9ec0360cc988596ce1b0834f9d39cea887911610
Nishant kumar <nishantkr2003nna@gmail.com>
2025-04-21 08:03:29 +0530

    Changes

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -5,37 +5,41 @@ public class InsertionSort {
 
     public static void main(String[] args) {
         int arr[] = {5,4,3,2,1,0,85,-12};
-        insertion(arr);
+        insertionSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
-    static void insertion(int arr[]){
-        for(int i=1;i<arr.length;i++){
-            int temp = arr[i];
-            int j = i-1;
-            while(j>=0 && arr[j]>temp){
-                arr[j+1] = arr[j];
-                j--;
-            }
-            arr[j+1] = temp;
-        }
-
-    }
-
-    // public static void insertionSort(int[] arr) {
+    // static void insertion(int arr[]){
     //     for(int i=1;i<arr.length;i++){
-    //         int curr = arr[i];
-    //         int prev = i-1;
-
-    //         //finding out the correct pos to insert
-    //         while(prev>=0 && arr[prev]>curr){
-    //             arr[prev+1] = arr[prev];
-    //             prev--;
+    //         int temp = arr[i];
+    //         int j = i-1;
+    //         while(j>=0 && arr[j]>temp){
+    //             arr[j+1] = arr[j];
+    //             j--;
     //         }
-    //         //inserting the element at the correct position
-
-    //         arr[prev+1] = arr[curr];
+    //         arr[j+1] = temp;
     //     }
+
     // }
+
+    public static void insertionSort(int[] arr) {
+    for (int i = 1; i < arr.length; i++) {
+        for (int j = i; j > 0; j--) {
+            if (arr[j] < arr[j - 1]) {
+                // Swap
+                swap(arr, j, j - 1);
+            } else {
+                break;
+            }
+        }
+    }
+}
+
+
+    public static void swap(int[] arr, int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
     
 }
