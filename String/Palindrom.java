729434adcf63294af4f1194d128539ada5c5661a
Nishant kumar <nishantkr2003nna@gmail.com>
2025-04-22 07:51:33 +0530

    Palindrom

---
+++ String/Palindrom.java
@@ -0,0 +1,28 @@
+public class Palindrom {
+    public static void main(String[] args) {
+        String str = "madam";
+        System.out.println(isPalindrome(str)); // true
+
+
+    }
+
+    static boolean isPalindrome(String str){
+        if (str == null || str.length() == 0){
+            return true;
+        }
+
+        str = str.toLowerCase();
+
+        for(int i=0;i<str.length()/2;i++){
+            char left = str.charAt(i);
+            char right = str.charAt(str.length()-1-i);
+            
+            if(left != right){
+                return false;
+            }
+        }
+
+
+        return true;
+    }
+}
