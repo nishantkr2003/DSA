85164db7b35833368dbb0a716bd56f0baa2c75a5
Nishant kumar <nishantkr2003nna@gmail.com>
2025-04-24 22:45:40 +0530

    242

---
+++ LeetCode/242.ValidAnagram.java
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/valid-anagram/description/
+
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        char[] sArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
+
+        Arrays.sort(sArr);
+        Arrays.sort(tArr);
+
+        return Arrays.equals(sArr, tArr);
+    }
+}
