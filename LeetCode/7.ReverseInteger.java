7e8bb0ded12d61adc38bc971eabf2d71edd7b302
Nishant kumar <nishantkr2003nna@gmail.com>
2025-05-07 16:38:01 +0530

    7

---
+++ LeetCode/7.ReverseInteger.java
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/reverse-integer/description/
+
+
+class Solution {
+    public int reverse(int x) {
+        int reverseNum = 0;
+        while(x != 0){
+            int last = x % 10 ;
+            if(reverseNum<Integer.MIN_VALUE/10 || reverseNum>Integer.MAX_VALUE/10){
+                return 0;
+            }
+            reverseNum = reverseNum *10 + last ;
+            x=x/10;
+        }
+        return reverseNum ;
+    }
+}
