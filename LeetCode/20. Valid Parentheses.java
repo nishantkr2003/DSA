2b1f4ffb51328306c99dcdae9638660e09485c3a
Nishant kumar <nishantkr2003nna@gmail.com>
2025-05-26 20:19:45 +0530

    20

---
+++ LeetCode/20. Valid Parentheses.java
@@ -0,0 +1,17 @@
+//  https://leetcode.com/problems/valid-parentheses/description/
+
+
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack<Character>();
+        for(char c:s.toCharArray()){
+            if(c=='(') st.push(')');
+            else if(c=='{') st.push('}');
+            else if(c=='[') st.push(']');
+            else if(st.isEmpty() || st.pop()!=c)
+            return false;
+        }
+        return st.isEmpty();
+    }
+}
