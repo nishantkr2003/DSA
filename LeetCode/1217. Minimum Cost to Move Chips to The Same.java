a2ce2be3bb4a7bd18779e7d42b980ce5bd272b01
Nishant kumar <nishantkr2003nna@gmail.com>
2025-05-27 21:48:32 +0530

    1217

---
+++ LeetCode/1217. Minimum Cost to Move Chips to The Same.java
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/minimum-cost-to-move-chips-to-the-same-position/description/
+
+class Solution {
+    public int minCostToMoveChips(int[] position) {
+        int even = 0;
+        int odd = 0;
+
+    
+        for (int i = 0; i < position.length; i++) {
+            if (position[i] % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+
+        
+        return Math.min(even, odd);
+    }
+}
