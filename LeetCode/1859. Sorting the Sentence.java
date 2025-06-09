fc8bc13863898bcc056723a0295b1ea585ff119e
Nishant kumar <nishantkr2003nna@gmail.com>
2025-06-09 22:17:04 +0530

    1859

---
+++ LeetCode/1859. Sorting the Sentence.java
@@ -0,0 +1,16 @@
+//  https://leetcode.com/problems/sorting-the-sentence/description/
+
+
+class Solution {
+    public String sortSentence(String s) {
+         String[] words = s.split(" ");
+        String[] sorted = new String[words.length];
+
+        for (String word : words) {
+            int index = Character.getNumericValue(word.charAt(word.length() - 1)) - 1;
+            sorted[index] = word.substring(0, word.length() - 1);
+        }
+
+        return String.join(" ", sorted);
+    }
+}
