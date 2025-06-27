e12504bdc462fcd2af64e06a60430555586ea98a
Nishant kumar <nishantkr2003nna@gmail.com>
2025-06-27 11:37:05 +0530

    rotation

---
+++ CRT/rotation.java
@@ -0,0 +1,19 @@
+public class rotation {
+    public static void main(String[] args) {
+        String s1 = "malayalam";
+        String s2 = "alayalayamm";
+
+        if (isRotation(s1, s2)) {
+            System.out.println("s1 is a rotation of s2.");
+        } else {
+            System.out.println("s1 is NOT a rotation of s2.");
+        }
+    }
+
+    static boolean isRotation(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+        String combined = s2 + s2;
+        return combined.contains(s1);
+    }
+}
+
