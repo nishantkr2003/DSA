7303d5ed27960d8b51bb82909b00cb18402281c5
Nishant kumar <nishantkr2003nna@gmail.com>
2025-07-03 10:00:03 +0530

    ActivitySelection

---
+++ CRT/ActivitySelection.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class ActivitySelection {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+       
+        int[][] activities = new int[N][2];
+
+        // Read input as pairs
+        for (int i = 0; i < N; i++) {
+            activities[i][0] = sc.nextInt(); // start time
+            activities[i][1] = sc.nextInt(); // end time
+        }
+
+        // Sort by end time
+        Arrays.sort(activities, Comparator.comparingInt(a -> a[1]));
+
+        int count = 0;
+        int lastEnd = -1;
+
+        for (int i = 0; i < N; i++) {
+            if (activities[i][0] >= lastEnd) {
+                count++;
+                lastEnd = activities[i][1];
+            }
+        }
+        System.out.println("Maximum number of activities that can be performed: ");
+        System.out.println(count);
+    }
+}
