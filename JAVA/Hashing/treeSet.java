baa03a1c052ec8924509af8b569cff0b41480ce4
Nishant kumar <nishantkr2003nna@gmail.com>
2025-07-04 15:32:46 +0530

    treeSet

--- JAVA/Hashing/treeSet.java
+++ JAVA/Hashing/treeSet.java
@@ -1,5 +1,18 @@
 package Hashing;
+import java.util.*;
 
 public class treeSet {
-    
+    public static void main(String[] args) {
+        // Create a TreeSet object
+        TreeSet<String> ts = new TreeSet<>();
+
+        // Adding elements to the TreeSet
+        ts.add("Apple");
+        ts.add("Banana");
+        ts.add("Cherry");
+        ts.add("Date");
+        ts.add("Elderberry");
+
+        System.out.println("TreeSet: " + ts);
+    }
 }
