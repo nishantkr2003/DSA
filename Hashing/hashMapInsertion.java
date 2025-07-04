4c2a4eeaa2fc945007474da137d69d155774648b
Nishant kumar <nishantkr2003nna@gmail.com>
2025-07-04 10:40:56 +0530

    hashMapInsertion

---
+++ Hashing/hashMapInsertion.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class hashMapInsertion{
+    public static void main(String[] args) {
+        // Create a HashMap
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        map.put("India",1);
+        map.put("USA",2);
+        map.put("UK",3);
+        map.put("China",4);
+        map.put("Japan",5);
+
+        System.out.println("HashMap: " + map);
+
+        // Iteration
+        Set<String> keys = map.keySet();
+
+        for (String k : keys) {
+            System.out.println("Key: " + k + ", Value: " + map.get(k));
+        }
+    }
+}
