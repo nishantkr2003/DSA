cad56d790208669c37088264f1f037b7bbeb23ab
Nishant kumar <nishantkr2003nna@gmail.com>
2025-07-04 12:27:19 +0530

    treeMap

---
+++ Hashing/treeMap.java
@@ -0,0 +1,20 @@
+package Hashing;
+import java.util.*;
+
+public class treeMap {
+    public static void main(String[] args) {
+        TreeMap<String, Integer> map = new TreeMap<>();
+        // Insertion
+        map.put("India", 1);
+        map.put("USA", 2);
+        map.put("UK", 3);
+        map.put("China", 4);
+        map.put("Japan", 5);
+
+        System.out.println("TreeMap: " + map);
+
+
+
+
+    }
+}
