9f65aed354097104b0a1f0b7cfa6ac0a2abb4a18
Nishant kumar <nishantkr2003nna@gmail.com>
2025-07-04 15:31:25 +0530

    hasSet

---
+++ JAVA/Hashing/linkedHashSet.java
@@ -0,0 +1,21 @@
+package Hashing;
+import java.util.*;
+ 
+public class linkedHashSet {
+    public static void main(String[] args) {
+        // Creating a linked hash set
+        LinkedHashSet<String> lhs = new LinkedHashSet<>();
+
+        lhs.add("Apple");
+        lhs.add("Banana");
+        lhs.add("Cherry");
+        lhs.add("Date");
+        lhs.add("Elderberry");
+
+        System.out.println("LinkedHashSet: " + lhs);
+
+
+
+    }
+    
+}
