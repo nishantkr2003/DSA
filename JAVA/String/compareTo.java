c88eed2be132c0e2e94827e2e49ce89c47577023
Nishant kumar <nishantkr2003nna@gmail.com>
2025-07-05 14:55:21 +0530

    compareTo

---
+++ JAVA/String/compareTo.java
@@ -0,0 +1,25 @@
+// compareTo
+//compareToIgnoreCase
+//lexicographically 
+
+
+
+
+
+
+package String;
+import java.util.*;
+public class compareTo {
+    public static void main(String[] args) {
+        String[] s = {"apple", "banana", "cherry","mango"};
+
+        String largest = s[0];
+        for(int i=0;i<s.length;i++){
+            if(largest.compareTo(s[i])<0){
+                largest = s[i];
+            }
+        }
+        System.out.println("The largest string is: " + largest);
+    }
+    
+}
