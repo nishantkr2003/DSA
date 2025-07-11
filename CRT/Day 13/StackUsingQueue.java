68a136b3a754668511dcf74e98e548a8507ae3b8
Nishant kumar <nishantkr2003nna@gmail.com>
2025-07-11 11:53:45 +0530

    Queue

---
+++ CRT/Day 13/StackUsingQueue.java
@@ -0,0 +1,17 @@
+
+import java.util.LinkedList;
+import java.util.Queue;
+public class StackUsingQueue {
+    public Queue<Integer> q1;
+    public Queue<Integer> q2;
+
+
+    public StackUsingQueue() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+
+
+    
+    
+}
