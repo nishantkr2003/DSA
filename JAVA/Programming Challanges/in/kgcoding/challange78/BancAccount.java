d5f6b15a4844d25649a1f8bb8d5c6e53e2c8b63e
Nishant kumar <nishantkr2003nna@gmail.com>
2025-07-11 23:56:17 +0530

    challange 78

--- JAVA/Programming Challanges/in/kgcoding/challange78/BancAccount.java
+++ JAVA/Programming Challanges/in/kgcoding/challange78/BancAccount.java
@@ -1,5 +1,35 @@
 package in.kgcoding.challange78;
 
-public class BancAccount {
-    
+class BancAccount {
+
+    private String accountNumber;
+    private String accountHolderName;
+    private double balance;
+
+    public BancAccount(String accountNumber, String accountHolderName) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+    }
+
+    public void depositMoney(double money){
+        if(money<=0){
+            System.out.println("Money cannot be negative");
+        }
+        else{
+            balance += money;
+        }
+        
+    }
+
+    public double withdrawMoney(double money) {
+        if (money <= 0) {
+            System.out.println("Invalid Withdraw");
+        } else if (balance >= money) {
+            balance -= money;
+        } else {
+            money = balance;
+            balance = 0;
+        }
+        return money;
+    }
 }
--- JAVA/Programming Challanges/in/kgcoding/challange78/Customer.java
+++ JAVA/Programming Challanges/in/kgcoding/challange78/Customer.java
@@ -1,5 +1,16 @@
 package in.kgcoding.challange78;
 
 public class Customer {
+
+    public static void main(String[] args) {
+        BancAccount account = new BancAccount("123456789", "Nishant Kumar");
+
+        account.depositMoney(100);
+        System.out.println(account.withdrawMoney(200));
+        account.depositMoney(-40);
+        account.withdrawMoney(0);
+    }
+
+    
     
 }
