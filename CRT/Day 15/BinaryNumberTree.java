476f62c1e23d320ed47eb6949f4dc5608f1be8f5
Nishant kumar <nishantkr2003nna@gmail.com>
2025-07-14 10:58:35 +0530

    BinaryNumberTree

---
+++ CRT/Day 15/BinaryNumberTree.java
@@ -0,0 +1,26 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class BinaryNumberTree {
+    public static void generateBinaryNumbers(int n) {
+        Queue<String> queue = new LinkedList<>();
+        queue.offer ("1");
+
+            for (int i = 0; i < n; i++) {
+            String number = queue.poll();
+            System.out.println(number);
+
+            queue.offer(number + "0");
+            queue.offer(number + "1");
+        }
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("number: ");
+        int n = sc.nextInt();
+        System.out.println("Bunary number");
+        generateBinaryNumbers(n);
+    }
+}
