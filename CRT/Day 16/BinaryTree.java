3c241f1906d567face524f2e9827bbffd98b02a3
Nishant kumar <nishantkr2003nna@gmail.com>
2025-07-15 10:19:35 +0530

    public class BinaryTree{

---
+++ CRT/Day 16/BinaryTree.java
@@ -0,0 +1,57 @@
+
+
+class TreeNode{
+    int value;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int v){
+        value = v;
+    }
+}
+
+public class BinaryTree{
+    public void inOrder(TreeNode root){
+        if(root == null){
+            return;
+        }
+        inOrder(root.left);
+        inOrder(root.right);
+        System.out.print(root.value + " ");
+    }
+
+        public void preOrder(TreeNode root) {
+        if (root == null) return;
+        System.out.print(root.value + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    public void postOrder(TreeNode root) {
+        if (root == null) return;
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.value + " ");
+    }
+
+        public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        BinaryTree tree = new BinaryTree();
+        System.out.print("In-order traversal: ");
+        tree.inOrder(root);
+        System.out.println();
+
+        System.out.print("Pre-order traversal: ");
+        tree.preOrder(root);
+        System.out.println();
+
+        System.out.print("Post-order traversal: ");
+        tree.postOrder(root);
+        System.out.println();
+    }
+}
