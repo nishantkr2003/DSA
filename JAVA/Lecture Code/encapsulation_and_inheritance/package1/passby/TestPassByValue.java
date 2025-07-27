cd3aab4ad4d08cfe77131558cefa3f881c3a2efc
Nishant kumar <nishantkr2003nna@gmail.com>
2025-07-27 07:36:10 +0530

    TestPassByValue

---
+++ JAVA/Lecture Code/encapsulation_and_inheritance/package1/passby/TestPassByValue.java
@@ -0,0 +1,13 @@
+
+public class TestPassByValue {
+    public static void main(String[] args) {
+        int x=5;
+        int y=10;
+        int sum = add(x,y);
+    }
+    public static int add(int a,int b){
+        return a+b;
+    }
+
+
+}
