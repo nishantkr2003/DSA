9a8215caee58ad9af9923569e222b4c8570b0362
Nishant kumar <nishantkr2003nna@gmail.com>
2025-07-27 08:17:14 +0530

    challange 86

---
+++ JAVA/Programming Challanges/in/kgcoding/challange86/Test.java
@@ -0,0 +1,9 @@
+
+public class Test {
+    public static void main(String[] args) {
+        Car c = new Car();
+        Vehicle v = new Vehicle();
+        v.service();
+        c.service();
+    }
+}
---
+++ JAVA/Programming Challanges/in/kgcoding/challange86/Vehicle.java
@@ -0,0 +1,6 @@
+
+public class Vehicle {
+    public void service(){
+        System.out.println("vehicle is getting serviced...");
+    }
+}
