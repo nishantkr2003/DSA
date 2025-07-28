c6f3eee6390c685d6fca15cd0160de344a9f7fdb
Nishant kumar <nishantkr2003nna@gmail.com>
2025-07-29 00:11:19 +0530

    Hello

--- JAVA/Hello.java
+++ JAVA/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     
     public static void main(String args[]){
-        System.out.println("Nishant Kumar Starts Learning DSA...");
+        System.out.println("Nishant Kumar starts Learning DSA...");
     }
 }
  
