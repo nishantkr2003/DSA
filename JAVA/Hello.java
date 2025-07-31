c54a9c2ee562b586e03965d6559d1eb55d52d547
Nishant kumar <nishantkr2003nna@gmail.com>
2025-07-31 22:59:03 +0530

    commit

--- JAVA/Hello.java
+++ JAVA/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter name:");
         String name = sc.nextLine();
+        System.out.println("Nishant Kumar starts Learning DSA...");
         
     }
 }
