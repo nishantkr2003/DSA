84eeba62cafebc7e6f3b1e67f38cb0e8654f0cfb
Nishant kumar <nishantkr2003nna@gmail.com>
2025-08-01 22:40:15 +0530

    ArrayListOperations

--- JAVA/ArrayList/ArrayListOperations.java
+++ JAVA/ArrayList/ArrayListOperations.java
@@ -4,11 +4,28 @@ public class ArrayListOperations {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
 
+        // add operation -- O(1)
         list.add(1);
         list.add(2);
         list.add(3);
         list.add(4);
         list.add(5);
         System.out.println(list);
+
+        // Get operation -- O(1)
+        int element = list.get(2); // index-2
+        System.out.println("Element at index 2: " + element);
+
+        // Delete element -- O(n)
+        list.remove(2);  // index-2
+        System.out.println(list);
+
+        // Set element -- O(n)
+        list.set(2,10); // (index,value)
+        System.out.println(list);
+
+        //contains -- O(n)
+        System.out.println(list.contains(1));
+        System.out.println(list.contains(11));
     }
 }
