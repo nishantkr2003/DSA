0a101b347d2659aa98d584f92e2b4036206749cc
Nishant kumar <nishantkr2003nna@gmail.com>
2025-08-02 00:03:43 +0530

    ContainerWithMostWater

--- JAVA/ArrayList/ContainerWithMostWater.java
+++ JAVA/ArrayList/ContainerWithMostWater.java
@@ -19,7 +19,29 @@ public class ContainerWithMostWater {
 
 
 
-    
+    // -- O(n)
+    public static int storeWater (ArrayList<Integer> height) {
+        int maxWater = 0;
+        int lp = 0;
+        int rp = height.size()-1;
+        while(lp < rp) {
+            //calculate water area
+            int ht = Math.min(height.get(lp), height.get(rp));
+            int width = rp-lp;
+            int currWater = ht * width;
+            maxWater = Math.max(maxWater, currWater);
+
+            //update ptr
+            if(height.get(lp) < height.get(rp)){
+                lp++;
+            }
+            else{
+                rp--;
+            }
+        }
+        return maxWater;
+    }
+
     public static void main(String[] args) {
         ArrayList<Integer> height = new ArrayList<>();
 
