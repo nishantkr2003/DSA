1a122d07c3c42e061e4349e8febbcb7f79adf3e6
Nishant kumar <nishantkr2003nna@gmail.com>
2025-08-03 10:09:32 +0530

    229

---
+++ LeetCode/LeetCode Questions/229. Majority Element II.java
@@ -0,0 +1,24 @@
+//  https://leetcode.com/problems/majority-element-ii/description/
+
+
+import java.util.*;
+
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        int n = nums.length;
+        List<Integer> result = new ArrayList<>();
+
+        for (int num : nums) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        for (int key : freqMap.keySet()) {
+            if (freqMap.get(key) > n / 3) {
+                result.add(key);
+            }
+        }
+
+        return result;
+    }
+}
