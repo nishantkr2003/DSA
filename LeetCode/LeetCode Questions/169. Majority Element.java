6bee05b77783a891097f85cf73259db92923cc04
Nishant kumar <nishantkr2003nna@gmail.com>
2025-08-03 09:43:23 +0530

    169

---
+++ LeetCode/LeetCode Questions/169. Majority Element.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/majority-element/description/
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int majorityElement = nums.length / 2;
+
+        for(int num : nums){
+            map.put(num ,map.getOrDefault(num,0)+1);
+
+
+            if(map.get(num) > majorityElement){
+                return num;
+            }
+        }
+        return -1;
+    }
+}
