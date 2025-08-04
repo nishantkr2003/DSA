707ec7ea88c417ee2626c26088493e6f7de4e8e3
Nishant kumar <nishantkr2003nna@gmail.com>
2025-08-04 14:21:44 +0530

    reverse

---
+++ JAVA/LinkedList/Reverse.java
@@ -0,0 +1,68 @@
+public class Reverse {
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+
+    public void addFirst(int data){
+        Node newNode = new Node(data);
+        size++;
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void reverse(){
+        Node prev = null;
+        Node curr = tail = head;
+        Node next;
+
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+
+
+    public void print(){
+        if(head == null){
+            System.out.println("Empty list");
+            return;
+        }
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        Reverse l = new Reverse();
+        l.addFirst(5);
+        l.addFirst(4);
+        l.addFirst(3);
+        l.addFirst(2);
+        l.addFirst(1);
+        l.addFirst(0);
+        l.print();
+        l.reverse();
+        l.print();
+    }
+}
