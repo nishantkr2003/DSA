597ad387c8a9f7e8320a97c31ea84b48d6394f2d
Nishant kumar <nishantkr2003nna@gmail.com>
2025-08-05 20:29:57 +0530

    Remove cycle

--- JAVA/LinkedList/Cycle.java
+++ JAVA/LinkedList/Cycle.java
@@ -29,14 +29,48 @@ public class Cycle {
         }
         return false; 
     }
+    
+    public static void removeCycle(){
+        Node slow = head;
+        Node fast = head;
+        boolean cycle = false;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                // found cycle, now remove it
+                cycle = true;
+                break;
+            }
+        }
+        if(cycle == false){
+            return; // no cycle found
+        }
+
+        // find meeting point
+        slow = head;
+        Node prev = null;
+    
+        while(slow != fast){
+            prev = fast;
+            slow = slow.next;
+            fast = fast.next;
+        }
+        // remove cycle
+        prev.next = null;
+    }
 
 
     public static void main(String[] args) {
         head = new Node(1);
         head.next = new Node(2);
         head.next.next = new Node(3);
-        head.next.next.next = head;
-        //head.next = new Node(2);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = head.next;
+        System.out.println(isCycle());
+        removeCycle();
         System.out.println(isCycle());
     }
 }
+
