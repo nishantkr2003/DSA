1da20641d3c3e6ae9c4fef7ef72e2ab1c53495bc
Nishant kumar <nishantkr2003nna@gmail.com>
2025-08-08 22:12:18 +0530

    DoubleLL

---
+++ JAVA/LinkedList/DoubleLL.java
@@ -0,0 +1,72 @@
+
+public class DoubleLL {
+    public class Node{
+        int data;
+        Node next;
+        Node prev;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    // add node
+    public void adFirst(int data){
+        Node newNode = new Node(data);
+        size++;
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+    }
+
+    public int removeFirst(){
+        if(head ==null){
+            System.out.println("Empty");
+            return Integer.MIN_VALUE;
+        }
+
+        if(size == 1){
+            int val = head.data;
+            head = tail = null;
+            size--;
+            return val;
+        }
+
+
+        int val = head.data;
+        head = head.next;
+        head.prev = null;
+        size--;
+        return val;
+    }
+
+    public void print(){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data + "<->");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+
+    public static void main(String[] args) {
+        DoubleLL dll = new DoubleLL();
+        dll.adFirst(3);
+        dll.adFirst(2);
+        dll.adFirst(1);
+        dll.print();
+        
+    }
+}
