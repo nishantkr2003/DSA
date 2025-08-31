43c2e742abc3dbb13aec59eddcda92f6a18759d0
Nishant kumar <nishantkr2003nna@gmail.com>
2025-08-31 19:42:00 +0530

    414

---
+++ LeetCode/LeetCode Questions/414. Third Maximum Number.java
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/third-maximum-number/description/?envType=problem-list-v2&envId=array
+
+
+import java.util.Arrays;
+
+class Solution {
+    public int thirdMax(int[] nums) {
+        Arrays.sort(nums);
+
+        int count = 1;
+        int prev = nums[nums.length - 1];
+
+        for(int i = nums.length - 2; i >= 0; i--){
+            if(nums[i] != prev){
+                count++;
+                prev = nums[i];
+            }
+
+            if(count == 3){
+            return nums[i];
+           }
+        }
+      return nums[nums.length - 1];
+    }
+}
