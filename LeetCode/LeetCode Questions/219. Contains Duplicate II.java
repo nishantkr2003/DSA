9deefc842d43aae7aea7fa6fc9c225bc7cbb64b5
Nishant kumar <nishantkr2003nna@gmail.com>
2025-09-01 21:36:55 +0530

    219

---
+++ LeetCode/LeetCode Questions/219. Contains Duplicate II.java
@@ -0,0 +1,20 @@
+
+// https://leetcode.com/problems/contains-duplicate-ii/?envType=problem-list-v2&envId=nhmw68pg
+
+
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                int index = map.get(nums[i]);
+                if(i-index<=k){
+                    return true;
+                }
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+}
