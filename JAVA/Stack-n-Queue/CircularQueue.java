b75cbb6e96b741c19fe6aa35b0bfcd9d6ccfcb25
Nishant kumar <nishantkr2003nna@gmail.com>
2025-09-07 00:59:03 +0530

    CircularQueue

--- JAVA/Stack-n-Queue/CircularQueue.java
+++ JAVA/Stack-n-Queue/CircularQueue.java
@@ -3,14 +3,19 @@ public class CircularQueue {
         static int arr[];
         static int size;
         static int rear;
+        static int front;
 
         Queue(int n){
             arr = new int[n];
             size = n;
             rear = -1;
+            front = -1;
         }
         public static boolean isEmpty(){
-            return rear  == -1;
+            return rear  == -1 && front == -1;
+        }
+        public static boolean isFull(){
+            return (rear+1)%size == front;
         }
 
 
@@ -20,7 +25,10 @@ public class CircularQueue {
                 System.out.println("Empty");
                 return;
             }
-            rear = rear + 1;
+            if(front==-1){
+                front=0;
+            }
+            rear = (rear + 1)%size;
             arr[rear] = data;
         }
 
@@ -31,12 +39,16 @@ public class CircularQueue {
                 System.out.println("Empty queue");
                 return -1;
             }
-            int front = arr[0];
-            for(int i=0;i<rear;i++){
-                arr[i] = arr[i+1];
+            int result = arr[front];
+            
+            
+            if(rear == front){
+                rear = front = -1;
             }
-            rear = rear-1;
-            return front;
+            else{
+                front =(front+1)%size;
+            }
+            return result;
 
         }
 
@@ -46,7 +58,7 @@ public class CircularQueue {
                 System.out.println("Empty queue");
                 return -1;
             }
-            return arr[0];
+            return arr[front];
         }
     }
 
@@ -56,6 +68,12 @@ public class CircularQueue {
         q.add(1);
         q.add(2);
         q.add(3);
+        System.out.println(q.remove());
+        q.add(4);
+        System.out.println(q.remove());
+        q.add(5);
+
+
 
         while(!q.isEmpty()){
             System.out.println(q.peek());
