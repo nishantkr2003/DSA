f8c3d9d2df098b94960624de40be4a4501e20fa0
Nishant kumar <nishantkr2003nna@gmail.com>
2025-09-09 21:21:31 +0530

    287

---
+++ LeetCode/LeetCode Questions/287. Find the Duplicate Number.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/find-the-duplicate-number/
+
+import java.util.*;
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                return num;  
+            } else {
+                map.put(num, 1); 
+            }
+        }
+        return -1;
+    }
+}
